3df10c87ee5b31acdab54cce54afb5fd40a2b255
oyenugae <oyenugae@babcock.edu.ng>
2020-11-12 08:26:55 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    char[][] magicSquare = {
+        {'P', 'R', 'E', 'Y'},
+        {'L', 'A', 'V', 'A'},
+        {'O', 'V', 'E', 'R'},{'T','E','N','D'}
+           };
+    System.out.println("Input the row you plan to check its word");
+    int rowNumber = scan.nextInt();
+    displayRow(magicSquare, rowNumber);
+    
+
+  }
+  static void displayRow(char[][]mSquare, int row){
+    System.out.printf("The word in row %d is ", row);
+      for (int i = 0; i<4; i++){
+
+         System.out.print(mSquare[row][i]);    
+      }
+    }  
+}
